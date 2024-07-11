10007d79f08f59552fd3bb03ae4971aae5a25ac3
Bettina <50010683+BettinaMaria98@users.noreply.github.com>
2024-07-11 10:49:00 +0200

    Update Controller.php
     Update SchemaKey to allow the nullable value to match the construct declaration

--- src/Controller.php
+++ src/Controller.php
@@ -52,7 +52,7 @@ class Controller extends BaseController
         'Max-Age' => 86400, // 86,400 seconds = 1 day.
     ];
 
-    private string $schemaKey;
+    private ?string $schemaKey;
 
     private QueryHandlerInterface $queryHandler;
 
@@ -390,7 +390,7 @@ class Controller extends BaseController
         return $member;
     }
 
-    public function setSchemaKey(string $schemaKey): Controller
+    public function setSchemaKey(?string $schemaKey): self
     {
         $this->schemaKey = $schemaKey;
         return $this;
